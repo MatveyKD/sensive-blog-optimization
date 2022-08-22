38b59eb1f0ff1d8ee9c1c901078255239778897e
Matvey256 <97842332+MatveyKD@users.noreply.github.com>
2022-08-22 10:27:36 +0300

    cleanup the code

--- blog/views.py
+++ blog/views.py
@@ -57,7 +57,7 @@ def index(request):
 
 
 def post_detail(request, slug):
-    post = Post.objects.get(slug=slug)
+    post = Post.objects.popular().get(slug=slug)
     serialized_comments = []
     for comment in post.comments.all().prefetch_related('author'):
         serialized_comments.append({
@@ -66,7 +66,7 @@ def post_detail(request, slug):
             'author': comment.author.username,
         })
 
-    likes = post.likes.all()
+    likes = post.likes_count
 
     related_tags = post.tags.annotate(posts_count = Count("posts"))
 
@@ -75,7 +75,7 @@ def post_detail(request, slug):
         'text': post.text,
         'author': post.author.username,
         'comments': serialized_comments,
-        'likes_amount': len(likes),
+        'likes_amount': likes,
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
